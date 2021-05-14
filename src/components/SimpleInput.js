add8d05b58702722cc81da4b3ac441c261ef36f7
aimon7 <air.ioannis@gmail.com>
2021-05-14 21:37:28 +0300

    Dealing With Form Submission & Getting User Input Values

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,46 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    // We don't use both
+    // Ref is better if you need validation in the submission (once)
+    const nameInputRef = useRef();
+    // State is better when you need validation in every keystroke and to reset the field
+    const [enteredName, setEnteredName] = useState('');
+
+    const nameInputChangeHandler = event => {
+        setEnteredName(event.target.value);
+    }
+
+    const formSubmissionHandler = event => {
+        event.preventDefault();
+
+        console.log(enteredName);
+        const enteredValue = nameInputRef.current.value;
+        console.log(enteredValue);
+
+        setEnteredName('');
+        // For ref:
+        // nameInputRef.current.value = ''
+        // but it is not ideal, because we are directly manipulating the DOM
+    }
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className="form-control">
+                <label htmlFor="name">Your Name</label>
+                <input
+                    ref={nameInputRef}
+                    type="text"
+                    id="name"
+                    onChange={nameInputChangeHandler}
+                    value={enteredName}
+                />
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
